e58eb3a1b047fb66205fbc40b8b81e11a9588171
Dmitry Petrov <61933931+dmitrypetrov-dev@users.noreply.github.com>
2020-11-23 14:40:34 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// jquery slider
+
+$(document).ready(function () {
+        $('.slider').slick({
+            arrows: true,
+            dots: false,
+            infinite: false,
+            fade: false,
+            autoplay: false,
+        });
+});
+
+
+
+
+
+
+
